fe54011f8c011d6bf2c42707968bed24505ad889
Vasil Dininski <dininski@gmail.com>
2013-05-14 13:17:26 +0200

    solved pr.25. 1000-digit Fibonacci number

---
+++ 25 - 1000-digit Fibonacci number/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static BigInteger[] fibNumbers = new BigInteger[100000];
+
+    public static BigInteger Fib(int index)
+    {
+        if (index == 0)
+        {
+            return fibNumbers[0];
+        }
+
+        if (fibNumbers[index] == 0)
+        {
+            fibNumbers[index] = Fib(index - 1) + Fib(index - 2);
+        }
+
+        return fibNumbers[index];
+    }
+
+    static void Main(string[] args)
+    {
+        fibNumbers[0] = 1;
+        fibNumbers[1] = 1;
+        int currentNumber = 0;
+        int digitsCount = 1;
+        while (digitsCount != 1000)
+        {
+            Fib(currentNumber);
+            digitsCount = fibNumbers[currentNumber].ToString().Length;
+            currentNumber++;
+        }
+
+        Console.WriteLine(currentNumber);
+    }
+}
