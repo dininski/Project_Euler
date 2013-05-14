07f666391be2924b51cfeccfa8f47ce924cfa70e
Vasil Dininski <dininski@gmail.com>
2013-05-14 14:22:23 +0200

    Started problem 28

---
+++ 28 - Number spiral diagonals/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int endingNumber = 1001;
+        double sum = 0;
+        int startingNumber = 2;
+        for (int i = 0; i < endingNumber; i++)
+        {
+            for (int j = 0; j < 8; j += 2)
+            {
+                sum += startingNumber * endingNumber-i;
+                startingNumber += j;
+            }
+        }
+        sum += 4 * endingNumber + 1;
+        Console.WriteLine(sum);
+    }
+}
