7717bab2cafe8a9ef3cf1234a03d777b52104101
Vasil Dininski <dininski@gmail.com>
2013-05-14 13:46:45 +0200

    solder pr. 21 - Amicable numbers

---
+++ 21 - Amicable numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static int CalculateSumOfProperDivisors(int number)
+    {
+        int sum = 0;
+        for (int divisor = 1; divisor <= number / 2; divisor++)
+        {
+            if (number % divisor == 0)
+            {
+                sum += divisor;
+            }
+        }
+
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        for (int currentNumber = 2; currentNumber < 10000; currentNumber++)
+        {
+            int sumOfDivisors = CalculateSumOfProperDivisors(currentNumber);
+            int amicableCalculation = CalculateSumOfProperDivisors(sumOfDivisors);
+            if (currentNumber == amicableCalculation && currentNumber!=sumOfDivisors)
+            {
+                sum += currentNumber + sumOfDivisors;
+            }
+        }
+
+        Console.WriteLine(sum / 2);
+    }
+}
