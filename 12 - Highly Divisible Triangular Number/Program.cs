7579e6945a16aea491604b30bc7a4ce41636268b
Vasil Dininski <dininski@gmail.com>
2013-05-21 09:35:56 +0200

    Solution for problem 12

---
+++ 12 - Highly Divisible Triangular Number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int currentTriangularNumberPosition = 2;
+        double currentTriangularNumber = 1;
+        int numberOfDivisors = 0;
+
+        while (numberOfDivisors < 500)
+        {
+            numberOfDivisors = FindNumberOfDivisor(currentTriangularNumber);
+            //            Console.WriteLine("{0} divisors count: {1}", currentTriangularNumber, numberOfDivisors);
+            currentTriangularNumber += currentTriangularNumberPosition;
+            currentTriangularNumberPosition++;
+        }
+        Console.WriteLine("{0} divisors count: {1}", currentTriangularNumber - currentTriangularNumberPosition + 1, numberOfDivisors);
+    }
+
+    public static int FindNumberOfDivisor(double number)
+    {
+        if (number == 1)
+        {
+            return 1;
+        }
+
+        int divisorsCount = 2;
+
+        for (int divisor = 1; divisor < (int)Math.Sqrt(number); divisor++)
+        {
+            if (number % divisor == 0)
+            {
+                divisorsCount += 2;
+            }
+        }
+
+        return divisorsCount;
+    }
+}
