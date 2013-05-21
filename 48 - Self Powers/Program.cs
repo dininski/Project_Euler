9fc26f553632faf5c9bf85ee8946bc5463c8b12d
Vasil Dininski <dininski@gmail.com>
2013-05-21 10:35:42 +0200

    Solution for problem 48

---
+++ 48 - Self Powers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        BigInteger result = 0;
+        for (int i = 0; i < 1001; i++)
+        {
+            BigInteger numberRaised = i;
+            for (int j = 0; j < i - 1; j++)
+            {
+                numberRaised *= i;
+            }
+
+            //Console.WriteLine(numberRaised);
+            BigInteger lastTenDigits = numberRaised % 10000000000;
+            //Console.WriteLine(lastTenDigits);
+            result += lastTenDigits;
+        }
+
+        Console.WriteLine(result % 10000000000);
+    }
+}
