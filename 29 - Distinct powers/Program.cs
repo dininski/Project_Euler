9a4c19b6b507d8e26c303722735a289e70d37b2f
Vasil Dininski <dininski@gmail.com>
2013-05-21 10:17:40 +0200

    Solution for problem 29

---
+++ 29 - Distinct powers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        SortedSet<double> numbers = new SortedSet<double>();
+
+        for (int a = 2; a <= 100; a++)
+        {
+            for (int b = 2; b <= 100; b++)
+            {
+                double result = Math.Pow(a, b);
+                if (!numbers.Contains(result))
+                {
+                    
+                }
+                numbers.Add(result);
+            }
+        }
+
+        Console.WriteLine(numbers.Count);
+    }
+}
